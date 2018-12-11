9bf5dc9b2ccf95c6172c774b732b40c54fca68b1
Anurag Kumar <anuragak@users.noreply.github.com>
2018-12-11 09:25:59 +0000

    Updates

---
+++ precache-manifest.fe3d5aa2c3859bf31af787f82f56efe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58db14a1c0e0e44501c3",
+    "url": "/Todo-App/static/js/runtime~main.58db14a1.js"
+  },
+  {
+    "revision": "c453af7801b4667c4b50",
+    "url": "/Todo-App/static/js/main.c453af78.chunk.js"
+  },
+  {
+    "revision": "5a6ed70850a997d23ee2",
+    "url": "/Todo-App/static/js/1.5a6ed708.chunk.js"
+  },
+  {
+    "revision": "c453af7801b4667c4b50",
+    "url": "/Todo-App/static/css/main.69b5c60b.chunk.css"
+  },
+  {
+    "revision": "7967bba3098e0a623021da34cbb4fdd9",
+    "url": "/Todo-App/index.html"
+  }
+];
